0461d70c74a223ec53be9bee2142f77de9675ef8
Gyanendra Mishra <anomaly.the@gmail.com>
2016-03-03 02:57:52 +0530

    compiles :)

--- lexerDef.h
+++ lexerDef.h
@@ -4,15 +4,23 @@
 // 2013A7PS151P Prabhjyot Singh Sodhi
 
 // lexerDef.h     : Contains all data definitions used in lexer.c
-#include "lexer.h"
 
+
+#ifndef LEXERDEF_H_INCLUDED
+#define LEXERDEF_H_INCLUDED
 typedef struct
 {
     int id;
-    char* name;
+    char const* name;
     int lineNo;
 }tokenInfo;
 
 int lineNo;
 int state;
 int offset;
+
+typedef int buffersize;
+typedef char* buffer;
+
+
+#endif
