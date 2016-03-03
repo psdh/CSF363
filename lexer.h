a751329c09d0d73ca1ebeec05d9412f888a82e1c
Gyanendra Mishra <anomaly.the@gmail.com>
2016-03-03 19:23:26 +0530

    works, atleast for test case 1 with buffer of size 1

--- lexer.h
+++ lexer.h
@@ -13,6 +13,8 @@
 
 FILE *getStream(FILE *fp, buffer B, buffersize k);
 
-tokenInfo  getNextToken(FILE *fp);
+// TODO<psdh> define tokenInfo (probably a struct that  will be used when storing in the symbol table)
+// Figure out where must tokenInfo be defined (which file that is)
+tokenInfo  getNextToken(FILE *fp, buffer b, buffersize k);
 
 #endif
--- lexerDef.h
+++ lexerDef.h
@@ -4,14 +4,14 @@
 // 2013A7PS151P Prabhjyot Singh Sodhi
 
 // lexerDef.h     : Contains all data definitions used in lexer.c
-
+#include<stdlib.h>
 
 #ifndef LEXERDEF_H_INCLUDED
 #define LEXERDEF_H_INCLUDED
 typedef struct
 {
     int id;
-    char const* name;
+    char * name;
     int lineNo;
 }tokenInfo;
 
@@ -21,6 +21,8 @@ int offset;
 
 typedef int buffersize;
 typedef char* buffer;
+
+buffer 	b = (buffer) malloc(100*sizeof(buffer));
 
 
 #endif
