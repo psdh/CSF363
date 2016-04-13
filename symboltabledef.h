00595d843d0f039ed7e1bcc8034ea5824d7e4613
Gyanendra Mishra <anomaly.the@gmail.com>
2016-04-14 00:40:41 +0530

    blank symboltabledef.h

---
+++ symboltabledef.h
@@ -0,0 +1,18 @@
+/*
+    Batch No: 47
+    Gyanendra Mishra 2013A7PS126P
+    Prabhjyot Singh Sodhi 2013A7PS151P
+    Filename:symboltableDef.h
+*/
+
+
+#ifndef SYMBOLTABLEDEF_H_INCLUDED
+#define SYMBOLTABLEDEF_H_INCLUDED
+
+#include <stdio.h>
+
+#define table_row 60
+
+
+
+#endif
