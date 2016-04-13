435fc627c221bd89e0ed2678b1f8ec53a532d1c6
Gyanendra Mishra <anomaly.the@gmail.com>
2016-04-14 00:21:15 +0530

    modified parser.h to make symboltable.h

---
+++ symboltable.h
@@ -0,0 +1,24 @@
+/*
+    Batch No: 47
+    Gyanendra Mishra 2013A7PS126P
+    Prabhjyot Singh Sodhi 2013A7PS151P
+    Filename:symboltable.h
+*/
+
+
+#ifndef SYMBOL_H_INCLUDED
+#define SYMBOL_H_INCLUDED
+
+#include "symboltabledef.h"
+
+#include <stdio.h>
+
+
+void createSymbolTable();
+
+symbolTable parseAST();
+
+void printSymbolTable();
+
+void intializeTable();
+#endif
