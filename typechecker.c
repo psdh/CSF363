5caed5632cb88fead2db52006a8af05961e9f5f8
Gyanendra Mishra <anomaly.the@gmail.com>
2016-04-18 21:52:19 +0530

    added typechecker files

---
+++ typechecker.c
@@ -0,0 +1,20 @@
+/*
+    Batch No: 47
+    Gyanendra Mishra 2013A7PS126P
+    Prabhjyot Singh Sodhi 2013A7PS151P
+    Filename: typechecker.c
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <stdio.h>
+
+
+void typecheck(parseTree pt, hashtable st, int statementtype) {
+    if (type == 10) // io
+    else if (type == 1) // assignment statement
+    else if(type == 2) // iterative statement
+    else  if (type == 3) // conditional statement
+    else if (type == 4) // functional call statement
+}
---
+++ typechecker.h
@@ -0,0 +1,16 @@
+/*
+    Batch No: 47
+    Gyanendra Mishra 2013A7PS126P
+    Prabhjyot Singh Sodhi 2013A7PS151P
+    Filename: typechecker.h
+*/
+
+#ifndef TYPECHECKER_H_INCLUDED
+#define TYPECHECKER_H_INCLUDED
+
+#include "symboltable.h"
+
+void typecheck(parseTree pt, hashtable st);
+
+
+#endif
