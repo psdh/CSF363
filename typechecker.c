b026bc9a7d9cc1dc861e9bc3c4063e7d92c352c6
Prabhjyot Singh Sodhi <prabhjyotsingh95@gmail.com>
2016-04-19 12:13:33 +0530

    linearize stmt otherstmt in if branch

--- typechecker.c
+++ typechecker.c
@@ -11,15 +11,26 @@
 #include <stdio.h>
 #include "typechecker.h"
 
-void check_iterative_stmt(parseTree curr, hashtable *st, char* scope)
+// things to check here
+// 1) expression in if statment is boolean, done => by the parser
+// 2)
+// 3) call handle_stmts recursively on this, for otherstmts
+void check_conditional_stmt(parseTree curr, hashtable *st, char* scope)
 {
+    curr = curr->firstKid;
 
+    while (curr != NULL)
+    {
+        printf("%d\n", curr->id);
+
+        curr = curr->siblings;
+    }
 }
 
 void check_stmt(parseTree curr, hashtable *st, int type, char* scope)
 {
-    if (type == 1) // iterative statement
-        check_iterative_stmt(curr, st, scope);
+    if (type == 3) // iterative statement
+        check_conditional_stmt(curr, st, scope);
     // else if (type == 1) // assignment statement
     // else if(type == 10) // iostatment
     // else  if (type == 3) // conditional statement
