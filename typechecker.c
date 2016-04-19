a8535e8a0124460486ad80c8c76c077c2974d113
Gyanendra Mishra <anomaly.the@gmail.com>
2016-04-19 19:06:49 +0530

    no segmenation fault at least

--- typechecker.c
+++ typechecker.c
@@ -64,7 +64,9 @@ void check_factor(parseTree factor, hashtable *st, char *scope, char *type){
         printf("%s\n", "CALM AS POSSIBLE");
         parseTree temp = all->siblings;
         // Normal variable (not record)
+        // @ToDo Scalar natak
         if(temp == NULL || temp->firstKid == NULL){
+            printf("%d\n", all->id);
             printf("!!!!!!!!!%s!!!!!!!\n", all->lexeme);
             // records are dvisible by scalar :(
             entry *var = get(st, all->lexeme, scope);
@@ -140,7 +142,7 @@ void check_termprime(parseTree termprime, hashtable *st, char*scope, char* type)
     parseTree factor = hpo->siblings;
     parseTree tpp = factor->siblings;
 
-    if (factor->firstKid->id == 123){
+    if (factor->firstKid->id == 132){
         printf("%s\n", "AAAA");
         check_arith(factor->firstKid, st, scope, type);
     }
@@ -156,7 +158,8 @@ void check_termprime(parseTree termprime, hashtable *st, char*scope, char* type)
 void check_expprime(parseTree expprime, hashtable *st, char * scope, char * type){
     parseTree term = expprime->firstKid->siblings;
     parseTree expp = term->siblings;
-    if(expp->firstKid!=NULL){}
+
+    if(expp->firstKid!=NULL)
         check_expprime(expp, st, scope, type);
 
     parseTree factor = term->firstKid;
@@ -164,7 +167,7 @@ void check_expprime(parseTree expprime, hashtable *st, char * scope, char * type
 
     printf("%d\n", factor->firstKid->id);
 
-    if (factor->firstKid->id == 123){
+    if (factor->firstKid->id == 132){
         check_arith(factor->firstKid, st, scope, type);
     }
     else {
@@ -186,7 +189,7 @@ void check_arith(parseTree expr, hashtable *st, char * scope, char * type){
     parseTree factor = term->firstKid;
     parseTree termprime = factor->siblings;
     printf("%d\n", factor->firstKid->id);
-    if (factor->firstKid->id == 123){
+    if (factor->firstKid->id == 132){
         check_arith(factor->firstKid, st, scope, type);
     }
     // not another arithmetic expression
@@ -203,6 +206,7 @@ void check_arith(parseTree expr, hashtable *st, char * scope, char * type){
 
 
     if(expprime != NULL && expprime->firstKid != NULL){
+        printf("Expprime: %d\n", expprime->id);
         check_expprime(expprime, st, scope, type);
     }
 
