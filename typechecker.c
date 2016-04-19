316255793f145e4d6b279500d1f08437433937fd
Prabhjyot Singh Sodhi <prabhjyotsingh95@gmail.com>
2016-04-19 21:53:16 +0530

    io stmts works now first cut

--- typechecker.c
+++ typechecker.c
@@ -96,6 +96,82 @@ void traverse_all_boolean(parseTree curr, hashtable *ht, char *scope, char *chec
     }
 }
 
+void traverse_all_write(parseTree curr, hashtable *ht, char *scope)
+{
+    if (curr == NULL)
+        return;
+
+    if(curr->firstKid != NULL)
+    {
+        traverse_all(curr->firstKid, ht, scope);
+       // do  you thing again!
+    }
+    else
+    {
+        // TK_ID detected
+        if (curr->id == 4)
+        {
+            entry *found = get(ht, curr->lexeme, scope);
+
+            if (found == NULL)
+                found = get(ht, curr->lexeme, "global");
+
+            if (found == NULL)
+            {
+                printf("Error: Variable: %s undeclared in %s or global scope\n", curr->lexeme, scope);
+                return;
+            }
+
+            // record type
+            if (found->isRecordInstance == 1)
+            {
+                if (curr->siblings->firstKid == NULL)
+                {
+                    printf("Error: Cannot write to a record, must specify member\n");
+                    return;
+                }
+                else
+                {
+                    char *field = curr->siblings->firstKid->lexeme;
+
+                    // to get the members for a record
+                    found = get(ht, found->type, "global");
+
+                    record_dec *starter = found->record;
+
+                    int flag = 0;
+
+                    while (starter != NULL)
+                    {
+                        if (strcmp(starter->name, field) == 0)
+                        {
+                            flag = 1;
+                            printf("sab equal hai\n");
+                            break;
+                        }
+                        starter = starter->next;
+                    }
+                    if (flag == 0)
+                    {
+                        printf("Error: There is no field <%s> in record type <%s>", field, found->key);
+                        return;
+                    }
+                }
+            }
+
+            return;
+        }
+    }
+
+    parseTree prev = curr;
+    curr = curr->siblings;
+
+    if(curr != NULL)
+    {
+        return traverse_all(curr->firstKid, ht, scope);
+    }
+}
+
 
 // things to check here
 // 1) expression in if statment is boolean, Done => by the parser
@@ -150,14 +226,32 @@ void check_iterative_stmt(parseTree curr, hashtable *st, char* scope)
     }
 }
 
+void check_io_stmt(parseTree curr, hashtable *st, char* scope)
+{
+    printf("io: %d\n", curr->firstKid->id);
+
+    // read statement
+    if (curr->firstKid->id == 35)
+    {
+        parseTree srid = curr->firstKid->siblings->firstKid;
+        traverse_all_write(srid->firstKid, st, scope);
+    }
+    else
+    {
+        traverse_all_write(curr->firstKid->siblings->firstKid, st, scope);
+    }
+}
+
 void check_stmt(parseTree curr, hashtable *st, int type, char* scope)
 {
-    if (type == 1) // iterative statement
+    if (type == 1) // assignment statement
         check_assignment_stmt(curr, st, scope);
-    if (type == 3) // iterative statement
+    else if (type == 3) // conditional statement
         check_conditional_stmt(curr, st, scope);
-    if (type == 2)
+    else if (type == 2) // iterative statment
         check_iterative_stmt(curr, st, scope);
+    else if (type == 10) // IO statement
+        check_io_stmt(curr, st, scope);
     // else if (type == 1) // assignment statement
     // else if(type == 10) // iostatment
     // else  if (type == 3) // conditional statement
