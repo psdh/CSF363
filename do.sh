c70b69a5314f522ab15f10f998dec16383f5c4ab
Gyanendra Mishra <anomaly.the@gmail.com>
2016-04-22 13:36:45 +0530

    do --> run

--- do.sh
+++
@@ -1,5 +0,0 @@
-gcc -w -g -c codegen.c -o codegen.o
-gcc -w -g -c symboltable.c -o symboltable.o
-gcc -w -g -c typechecker.c -o typechecker.o
-gcc -w -g -c semantic.c -o semantic.o
-gcc -w -g codegen.o parser.o semantic.o symboltable.o typechecker.o lexer.o ast.c -o ast
---
+++ run.sh
@@ -0,0 +1,6 @@
+# Batch 47
+# Gyanendra Mishra 2013A7PS126P
+# Prabhjyot Singh Sodhi
+nasm -f elf64 code.asm
+gcc code.o
+./a.out
